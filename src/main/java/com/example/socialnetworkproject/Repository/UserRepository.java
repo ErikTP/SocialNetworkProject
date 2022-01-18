24c0a1201577350e834a52e6d7b12553521ec372
Erik Torres Puente <81220576+ErikTP@users.noreply.github.com>
2022-01-18 14:04:29 +0100

    Repository implementering
    Implementering av repository filer med namnen UserRepository och PostRepository. Metoder inuti kommunicerar med databasen för att hämta användarinformation som lista med id, förnamn, efternamn och datum. En void metod tillkallas även för att radera användare.

---
+++ src/main/java/com/example/socialnetworkproject/Repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.socialnetworkproject.Repository;
+
+import com.example.socialnetworkproject.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+    User findByEmail(String email);
+
+}
